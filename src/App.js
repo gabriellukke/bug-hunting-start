1621f6e0374357939c0be0bb811c23d040952aa4
gabriellukke <gabriellukke@outlook.com>
2022-02-22 14:48:40 -0300

    Config: Eslint Trybe Frontend

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <h1>
-        Countries App
-      </h1>
-    )
+      <h1>Boilerplate</h1>
+    );
   }
 }
 
