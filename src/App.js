a1bc80ccc6a55ea4d1a671de20cb028ff3f41fa8
gabriellukke <gabriellukke@outlook.com>
2022-03-04 12:34:26 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
 import React from 'react';
+import InvoiceForm from './components/InvoiceForm';
+import Invoice from './components/Invoice';
+import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      address: '',
+      city: '',
+      invoiceItems: [],
+      invoiceList: [],
+    };
+
+    this.handleCustomerChange = this.handleCustomerChange.bind(this);
+    this.saveInvoice = this.saveInvoice.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleCustomerChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  addItem(e, item) {
+    e.preventDefault();
+    this.setState((prevState) => ({ invoiceItems: [...prevState.invoiceItems, item] }));
+  }
+
+  saveInvoice() {
+    this.setState((prevState) => {
+      const { name, email, address, city, invoiceItems } = prevState;
+      const newInvoice = {
+        name,
+        email,
+        address,
+        city,
+        invoiceItems,
+      };
+
+      return {
+        invoiceList: [...prevState.invoiceList, newInvoice]
+      };
+    });
+  }
+
   render() {
+    const { invoiceItems, name, email, address, city } = this.state;
     return (
-      <h1>
-        React Boilerplate
-      </h1>
+      <main>
+        <InvoiceForm
+          name={name}
+          email={email}
+          address={address}
+          city={city}
+          handleCustomerChange={this.handleCustomerChange}
+          saveInvoice={this.saveInvoice}
+          addItem={this.addItem}
+        />
+        <Invoice
+          name={name}
+          email={email}
+          address={address}
+          city={city}
+          invoiceItems={invoiceItems}
+        />
+      </main>
     );
   }
 }
